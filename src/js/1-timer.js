911f5c2ac9be67aad8aa1f59f4bf0be83a1fecad
YunusOzkaya <nasuhan.yunus.ozkaya@gmail.com>
2025-05-05 16:08:05 +0300

    First commit

---
+++ src/js/1-timer.js
@@ -0,0 +1,71 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+let userSelectedDate;
+let startBtn = document.querySelector("button[data-start]");
+startBtn.disabled = true;
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+     userSelectedDate=selectedDates[0];
+     if(selectedDates[0] < new Date()) {
+        iziToast.error({
+            title: 'Hata',
+            message: 'Geçmiş bir tarih seçtin!',
+            position: 'topCenter',
+          });
+        return;
+      }
+      else {
+        document.querySelector("button[data-start]").disabled = false;
+        userSelectedDate = selectedDates[0];
+        console.log(userSelectedDate);
+      }
+    },
+  };
+
+  const flatpickrInstance = flatpickr("#datetime-picker", options);
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
+  
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
+startBtn.addEventListener("click", () => {
+    startBtn.disabled = true;
+    flatpickrInstance.destroy(0);
+    let timerId = setInterval(() => {
+        const currentTime = new Date();
+        const deltaTime = userSelectedDate - currentTime;
+        if (deltaTime < 0) {
+            clearInterval(timerId);
+            return;
+        }
+        const { days, hours, minutes, seconds } = convertMs(deltaTime);
+        document.querySelector("span[data-days]").textContent = addLeadingZero(days);
+        document.querySelector("span[data-hours]").textContent = addLeadingZero(hours);
+        document.querySelector("span[data-minutes]").textContent = addLeadingZero(minutes);
+        document.querySelector("span[data-seconds]").textContent = addLeadingZero(seconds);
+    }, 1000);
+}
+);
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,34 @@
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const delay = Number(document.querySelector('input[name="delay"]').value);
+  const state = document.querySelector('input[name="state"]:checked').value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then((delay) => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch((delay) => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
